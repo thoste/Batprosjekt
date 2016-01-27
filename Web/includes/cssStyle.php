b3a182835d59774eb3c7cb42c2501aed62e4661c
Thomas Stenseth <thomasstenseth@dhcp-016060.wlan.ntnu.no>
2016-01-27 13:38:02 +0100

    Removed webpage

--- Web/includes/cssStyle.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Denne filen inneholder lenker til to .css filer
-//Denne filen er inkludert i includer.php
-//Alle filer som inkluderer includer.php vil dermed ha tilgang til disse to .css filene
-?>
-
-<link rel="stylesheet" type="text/css" href="./includes/table.css">
-<link rel="stylesheet" type="text/css" href="./includes/mainStyle.css">
