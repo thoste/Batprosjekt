bc08b3f82fceffec5be095b484f6610f87df7e68
Thomas Stenseth <thomasstenseth@dhcp-017033.wlan.ntnu.no>
2016-02-23 10:08:07 +0100

    merged filter and misc

--- Arduino/filter.ino
+++ Arduino/filter.ino
@@ -11,3 +11,17 @@ double middle(double arr[], int size) {
 
 	delete[] temp;
 }
+
+void insSort(double arr[], int size) {
+	
+	int j, temp; 
+	for (int i = 0; i < size; i++) {
+		j = i;
+		while ((j > 0) && (arr[j] < arr[j - 1])) {
+			temp = arr[j];
+			arr[j] = arr[j - 1];
+			arr[j - 1] = temp;
+			j--;
+		}
+	}
+}
