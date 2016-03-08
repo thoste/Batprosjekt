e7ded5a386e8c8aca289fab384b82890ae94f116
Thomas Stenseth <thomasstenseth@dhcp-018120.wlan.ntnu.no>
2016-03-08 08:35:19 +0100

    removed old code

--- Arduino/cstring.h
+++
@@ -1,9 +0,0 @@
-//Returns length of a c-string
-int cstringLength(char* str);
-
-//Appends a char to a c-string
-void cstringAppend(char* str, char c);
-
-//Convert from c-string to integer
-int cstring2int(char* p);
-
